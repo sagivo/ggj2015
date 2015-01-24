518cbb746ab18611da7ba2fec88c6a7418012d1b
sagivo <sagiv4@gmail.com>
2015-01-24 15:02:54 -0500

    refactor server side

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var game_index = 1; var player_index = 1;
 var games = {}; //player1, player2, lvl
 var players = {}; //game_id, actions
-var line = [];
+var line = []; //list of players
 
 app.get('/', function(req, res) {
   res.send('hello world');
@@ -18,14 +18,13 @@ app.post('/init', function(req, res){
   if (line.length > 0) { //join
     var game_id = game_index++;
     var player2 = line.shift();
-    s += '|' + player2 + '|' + game_id;
     games[game_id] = {player1: player1, player2: player2, lvl: 1};
-    players[player1] = players[player2] = {game_id: game_id};
+    players[player1] = {game_id: game_id}; players[player2] = {game_id: game_id};
   } else { //wait
     line.push(player1);
     players[player1] = {game_id: null};
   }
-  res.send(s); // player1 | player2 | gameid
+  res.send(s); //player_id
 });
 
 app.get('/check/:player_id', function(req, res){
@@ -34,22 +33,22 @@ app.get('/check/:player_id', function(req, res){
 });
 
 app.post('/actions/:player_id', function(req, res){
-  players[req.player_id].actions = req.body.actions;
+  players[req.params.player_id].actions = req.body.actions;
   res.send('ok');
 });
 
 app.get('/actions/:game_id', function(req, res){
   var game = games[req.params.game_id];
   if (players[game.player1].actions && players[game.player2].actions)
-    s = game.player1 + '|' + players[game.player1].actions + "|" + game.player2 + players[game.player2].actions;
+    s = game.player1 + '|' + players[game.player1].actions + '|' + game.player2 + '|' + players[game.player2].actions;
   else s = 'wait';
-  res.send('ok');
+  res.send(s);
 });
 
 app.post('/lvlup/:game_id', function(req, res){
   var game = games[req.params.game_id];
   if (players[game.player1].actions){
-    players[game.player1].actions = players[game.player2].actions = null;
+    players[game.player1].actions = null; players[game.player2].actions = null;
     game.lvl++;
   }
   res.send('ok');
