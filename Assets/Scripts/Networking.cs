c7e54f729349ec9b12622cfab0976f73bcf992e2
sagivo <sagiv4@gmail.com>
2015-01-24 16:07:34 -0500

    refactor server side

--- Assets/Scripts/Networking.cs
+++ Assets/Scripts/Networking.cs
@@ -7,7 +7,8 @@ public class Networking : MonoBehaviour {
 	public System.Action<string> OnGetComplete;
 	public System.Action<string> OnPostComplete;
 
-	string baseUrl = "http://0.0.0.0:8080"; //"https://young-brushlands-8741.herokuapp.com"
+	string baseUrl = "https://young-brushlands-8741.herokuapp.com";
+	//string baseUrl = "http://0.0.0.0:8080";
 
 	void Start(){}
 	
