430fc273046d0b99cf5f2b221e8879ffb0cce359
sagivo <sagiv4@gmail.com>
2015-01-24 18:55:58 -0500

    s

--- server/server.js
+++ server/server.js
@@ -37,8 +37,10 @@ app.get('/check/:player_id', function(req, res){
 });
 
 app.post('/actions/:player_id', function(req, res){
+  if (players[req.params.player_id].actions) players[game.player1].actions = players[game.player2].actions = null;
   players[req.params.player_id].actions = req.body.actions;
   res.send('ok');
+  }
 });
 
 app.get('/actions/:game_id', function(req, res){
