7b08a750f7830fbcfcab43e8cde9848364bf9b74
sagivo <sagiv4@gmail.com>
2015-01-24 22:46:23 -0500

    refactor server side

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -3,22 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Test : MonoBehaviour {
-	float speed = 40;
+	float speed = 10;
 	Vector2 v;
 	// Use this for initialization
 	void Start () {
-	
+		v = Vector2.zero;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		v = Vector2.zero;
+		//v = Vector2.zero;
 		if (Input.GetKeyDown(KeyCode.DownArrow)) v = -Vector2.up;
 		else if (Input.GetKeyDown(KeyCode.UpArrow)) v = Vector2.up; 
 		else if (Input.GetKeyDown(KeyCode.RightArrow)) v = Vector2.right; 
 		else if (Input.GetKeyDown(KeyCode.LeftArrow)) v = -Vector2.right;
-		if (v!=Vector2.zero) {
-			transform.Translate(v * speed * Time.deltaTime);
-		}
+		//if (v!=Vector2.zero) {
+		//rigidbody2D.MovePosition(v);
+		transform.Translate(v * speed * Time.deltaTime);
+		//}
 	}
 }
