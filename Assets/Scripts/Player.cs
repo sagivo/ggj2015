6f9667b32ce3f654d66890533d78fd0c453f3a22
sagivo <sagiv4@gmail.com>
2015-01-24 20:43:49 -0500

    refactor server side

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour {
 	public GameObject player2Prefub;
 	public Text text;
 	int lvl = 1;
-	int[] lvlTime = new int[]{0,5,7,15,17};
+	int[] lvlTime = new int[]{0,10,7,15,17};
 	//string player2Id;
 	List<string> player2Records;
 	Vector3 startPos1;
@@ -165,7 +165,7 @@ public class Player : MonoBehaviour {
 				gameMode = gameModeType.Replay;
 				startReplayTime = Time.time;
 				text.text = "Replay";
-				Invoke("startRecording", lvlTime[lvl]+ 2);
+				Invoke("startRecording", lvlTime[lvl]+ 5);
 			}
 			break;		
 		default: break;
