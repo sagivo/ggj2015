eb9b9801654cb187184a840e54340f50c03b7dc1
sagivo <sagiv4@gmail.com>
2015-01-25 04:08:19 -0500

    refactor server side

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Test : MonoBehaviour {
+	public bool player1;
 	float speed = 10;
 	Vector2 v;
 	// Use this for initialization
@@ -13,10 +14,17 @@ public class Test : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		v = Vector2.zero;
-		if (Input.GetKeyDown(KeyCode.DownArrow)) v = -Vector2.up;
-		else if (Input.GetKeyDown(KeyCode.UpArrow)) v = Vector2.up; 
-		else if (Input.GetKeyDown(KeyCode.RightArrow)) v = Vector2.right; 
-		else if (Input.GetKeyDown(KeyCode.LeftArrow)) v = -Vector2.right;
+		if (player1){
+			if (Input.GetKeyDown(KeyCode.DownArrow)) v = -Vector2.up;
+			else if (Input.GetKeyDown(KeyCode.UpArrow)) v = Vector2.up; 
+			else if (Input.GetKeyDown(KeyCode.RightArrow)) v = Vector2.right; 
+			else if (Input.GetKeyDown(KeyCode.LeftArrow)) v = -Vector2.right;
+		} else {
+			if (Input.GetKeyDown(KeyCode.S)) v = -Vector2.up;
+			else if (Input.GetKeyDown(KeyCode.W)) v = Vector2.up; 
+			else if (Input.GetKeyDown(KeyCode.D)) v = Vector2.right; 
+			else if (Input.GetKeyDown(KeyCode.A)) v = -Vector2.right;
+		}
 		if (v!=Vector2.zero) {
 
 			//transform.Translate(v * speed * Time.deltaTime);
